6aaed0e5a519317859228782e20e24aa846e8e14
Thomas Hoek <thomas@tag-171.home>
2024-08-06 15:18:35 +0200

    Implement endpoints 1 and 2

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,23 @@
 from fastapi import APIRouter
 
+from .hacker_news_api import fetch_comments
+from .utils import count_n_most_common_words
+
 router = APIRouter()
 
 @router.get("/50-comments")
 async def get_top_50_comments():
     """Get first 50 comments to top 100 stories"""
-    pass
+    return fetch_comments(n_comments=50, from_n_stories=100)
 
 @router.get("/10-words")
 async def get_top_10_words():
     """Get 10 most used words in first 100 comments to top 30 stories"""
-    pass
+    comments = fetch_comments(n_comments=100, from_n_stories=30)
+    text = ''.join(comments)
+    top_10_words = count_n_most_common_words(text, 10)
+    print(top_10_words)
+    return top_10_words
 
 @router.get("/most-used-words")
 async def get_most_used_words():
