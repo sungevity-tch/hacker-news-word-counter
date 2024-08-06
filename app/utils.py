cd261eca8777f984af23eb8913330b567146a1ac
Thomas Hoek <thomas@tag-171.home>
2024-08-06 15:12:09 +0200

    Add word counter

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,27 @@
-from .hacker_news_api import fetch_best_stories, fetch_item, build_params
+import nltk
+from collections import Counter
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
+nltk.download('punkt')
+nltk.download('stopwords')
 
-def count_words():
-    """Count most used words"""
-    pass
+def count_n_most_common_words(text, n=10):
+
+    # Tokenize words
+    words = word_tokenize(text)
+
+    # Convert to lower case and remove punctuation
+    words = [word.lower() for word in words if word.isalpha()]
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    
+    # Count frequency of each word
+    word_counts = Counter(words)
+    
+    # Get n most common words
+    most_common_words = word_counts.most_common(n)
+    
+    return most_common_words
